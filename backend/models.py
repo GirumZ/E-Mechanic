b0b8ab1c9b6c57562ae3702900a47c6eff3f5d70
GirumZ <girum.zebe@gmail.com>
2025-01-16 00:22:28 +0300

    [Update]: BaseModel class removed from models

--- backend/models.py
+++ backend/models.py
@@ -1,16 +1,8 @@
 from app import db
-
-class BaseModel(db.Model):
-    """Base class for all models to avoid repetition of the 'id' column definition."""
-    __abstract__ = True
-
-    id = db.Column(db.Integer, primary_key=True)
-
-
-
-class Symptom(BaseModel):
+class Symptom(db.Model):
     __tablename__ = 'symptoms'
 
+    id = db.Column(db.Integer, primary_key=True)
     symptom_text = db.Column(db.String(255), nullable=False)
     sensation = db.Column(db.String(255), nullable=True)
     followup_available = db.Column(db.Boolean, nullable=False)
@@ -19,12 +11,13 @@ class Symptom(BaseModel):
     questions = db.relationship('Question', backref='symptom', lazy=True)
 
     def __repr__(self):
-        return f"<Symptom {self.symptem_text}>"
+        return f"<Symptom {self.symptom_text}>"
 
 
-class Question(BaseModel):
+class Question(db.Model):
     __tablename__ = 'questions'
 
+    id = db.Column(db.Integer, primary_key=True)
     question_text = db.Column(db.String(255), nullable=False)
     symptom_id = db.Column(db.Integer, db.ForeignKey('symptoms.id'), nullable=False)
 
@@ -35,9 +28,10 @@ class Question(BaseModel):
         return f"<Question {self.question_text}>"
 
 
-class Problem(BaseModel):
+class Problem(db.Model):
     __tablename__ = 'problems'
 
+    id = db.Column(db.Integer, primary_key=True)
     problem_description = db.Column(db.String(255), nullable=False)
     question_id = db.Column(db.Integer, db.ForeignKey('questions.id'), nullable=False)
 
@@ -45,18 +39,20 @@ class Problem(BaseModel):
         return f"<Problem {self.problem_description}>"
 
 
-class Tip(BaseModel):
+class Tip(db.Model):
     __tablename__ = 'tips'
 
+    id = db.Column(db.Integer, primary_key=True)
     tip_description = db.Column(db.String(255), nullable=False)
 
     def __repr__(self):
         return f"<Tip {self.tip_description}>"
 
 
-class OBDCode(BaseModel):
+class OBDCode(db.Model):
     __tablename__ = 'obd'
 
+    id = db.Column(db.Integer, primary_key=True)
     code = db.Column(db.String(50), nullable=False)
     description = db.Column(db.String(255), nullable=False)
 
