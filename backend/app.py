b7d8964f4a602aa510681170ad5d0e52064c2ae3
GirumZ <girum.zebe@gmail.com>
2025-01-15 22:57:39 +0300

    [Update]: Include the symptom blueprint

--- backend/app.py
+++ backend/app.py
@@ -2,15 +2,20 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
-# from models import db, Symptom, Question, Problem, Tip, OBDCode
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 from models import *
+
+from routes.symptom_routes import symptom_bp
+
+app.register_blueprint(symptom_bp)
 
 @app.route('/')
 def home():
