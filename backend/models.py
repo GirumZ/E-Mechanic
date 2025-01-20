c6f04d0c4c506dcac7dcd4f2d1038fa0eecd6e03
GirumZ <girum.zebe@gmail.com>
2025-01-20 16:00:37 +0300

    [Update]: made id field separate for each model and delete the BaseModel class

--- backend/models.py
+++ backend/models.py
@@ -2,7 +2,7 @@ from app import db
 class Symptom(db.Model):
     __tablename__ = 'symptoms'
 
-    id = db.Column(db.Integer, primary_key=True)
+    symptom_id = db.Column(db.Integer, primary_key=True)
     symptom_text = db.Column(db.String(255), nullable=False)
     sensation = db.Column(db.String(255), nullable=True)
     followup_available = db.Column(db.Boolean, nullable=False)
@@ -17,9 +17,9 @@ class Symptom(db.Model):
 class Question(db.Model):
     __tablename__ = 'questions'
 
-    id = db.Column(db.Integer, primary_key=True)
+    question_id = db.Column(db.Integer, primary_key=True)
     question_text = db.Column(db.String(255), nullable=False)
-    symptom_id = db.Column(db.Integer, db.ForeignKey('symptoms.id'), nullable=False)
+    symptom_id = db.Column(db.Integer, db.ForeignKey('symptoms.symptom_id'), nullable=False)
 
     # Relationship with Problem
     problems = db.relationship('Problem', backref='question', lazy=True)
@@ -31,10 +31,11 @@ class Question(db.Model):
 class Problem(db.Model):
     __tablename__ = 'problems'
 
-    id = db.Column(db.Integer, primary_key=True)
-    problem_description = db.Column(db.String(255), nullable=False)
-    question_id = db.Column(db.Integer, db.ForeignKey('questions.id'), nullable=False)
-
+    problem_id = db.Column(db.Integer, primary_key=True)
+    problem_description = db.Column(db.String(516), nullable=False)
+    solution = db.Column(db.String(516), nullable=False)
+    question_id = db.Column(db.Integer, db.ForeignKey('questions.question_id'), nullable=True)
+    symptom_id = db.Column(db.Integer, db.ForeignKey('symptoms.symptom_id'), nullable=False)
     def __repr__(self):
         return f"<Problem {self.problem_description}>"
 
@@ -42,8 +43,9 @@ class Problem(db.Model):
 class Tip(db.Model):
     __tablename__ = 'tips'
 
-    id = db.Column(db.Integer, primary_key=True)
-    tip_description = db.Column(db.String(255), nullable=False)
+    tip_id = db.Column(db.Integer, primary_key=True)
+    tip_short = db.Column(db.String(255), nullable=False)
+    tip_long = db.Column(db.String(255), nullable=False)
 
     def __repr__(self):
         return f"<Tip {self.tip_description}>"
@@ -52,9 +54,9 @@ class Tip(db.Model):
 class OBDCode(db.Model):
     __tablename__ = 'obd'
 
-    id = db.Column(db.Integer, primary_key=True)
-    code = db.Column(db.String(50), nullable=False)
-    description = db.Column(db.String(255), nullable=False)
+    obd_id = db.Column(db.Integer, primary_key=True)
+    obd_code = db.Column(db.String(50), nullable=False)
+    obd_description = db.Column(db.String(255), nullable=False)
 
     def __repr__(self):
         return f"<OBDCode {self.code}>"
